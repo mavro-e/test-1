f4f8f1197a348f60aad14a8a110095296a1b51e2
osboxes.org <mavro.bozicevic@ericsson.com>
2016-12-02 11:46:52 +0000

    Reformatting C and changing the message

--- asdf.c
+++ asdf.c
@@ -1,11 +1,10 @@
-#include<stdio.h>
-int main()
-{
-	int i; char e;
-	for(i=0;i<10;i++)
-		printf("*\n");
-	printf("The New Branch\n");
-	scanf("%c", &e);
-	return 0;
+#include <stdio.h>
+int main() {
+    int i;
+    char e;
+    for (i = 0; i < 10; i++)
+        printf("*\n");
+    printf("The New Branch\n");
+    scanf("%c", &e);
+    return 0;
 }
-
