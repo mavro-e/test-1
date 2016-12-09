fe2feb24dceb1e5ea0657aa081fb0e4b86cac9a1
osboxes.org <mavro.bozicevic@ericsson.com>
2016-12-09 12:50:34 +0000

    Adding

---
+++ asdf.c
@@ -0,0 +1,14 @@
+// uzt
+#include <stdio.h>
+int main() {
+    int i;
+    char e;
+    for (i = 0; i < 10; i++)
+        printf("*\n");
+    printf("The New Branch\n");
+    printf("C in fri212\n");
+    printf("Time Formatting a C/C++/Java File\n");
+
+    scanf("%c", &e);
+    return 0;
+}
